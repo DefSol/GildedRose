c839d2f82dd21df7f0ecf5b875a55879f87e3203
greglowe88 <glowe@fraedom.com>
2016-07-21 09:07:59 +1200

    Revert "initial push"
    This reverts commit 552860d9226d54f0fbff3714e31b654ad108a2d1.

--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -1,14 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using GildedRose.Console;
 using Xunit;
-using Xunit.Sdk;
 
 namespace GildedRose.Tests
 {
-   
     public class TestAssemblyTests
     {
         [Fact]
@@ -16,48 +9,5 @@ namespace GildedRose.Tests
         {
             Assert.True(true);
         }
-
-        [Fact]
-        public void TestCreateList()
-        {
-            var app = new Console.Program();
-            
-            Assert.True(app.Items.Count > 0);
-        }
-
-        [Fact]
-        public void TestItemsSellin()
-        {
-            var app = new Console.Program();
-
-            Assert.True(app.Items.Count(i=> i.SellIn < 0) == 0);
-        }
-
-
-        [Fact]
-        public void TestItemsQuantity()
-        {
-            var app = new Console.Program();
-
-            Assert.True(app.Items.Count(i => i.Quality < 0) == 0);
-        }
-
-
-        [Fact]
-        public void TestItemsUpdate()
-        {
-            var app = new Console.Program();
-            var listv1 = app.CreateItemList();         
-            app.UpdateQuality();
-            var listv2 = app.Items;
-
-            for (int i = 0; i < listv2.Count; i++)
-            {
-               Assert.True(listv1[i].SellIn !=  listv2[i].SellIn  && listv1[i].Name == listv2[i].Name , $"{listv1[i].Name} { listv1[i].SellIn } {listv2[i].SellIn}");
-            }
-
-        }
-       
     }
-
 }
