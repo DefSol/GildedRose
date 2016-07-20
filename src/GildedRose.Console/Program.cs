552860d9226d54f0fbff3714e31b654ad108a2d1
greglowe88 <glowe@fraedom.com>
2016-07-21 08:59:16 +1200

    initial push

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -2,37 +2,49 @@
 
 namespace GildedRose.Console
 {
-    class Program
+    public class Program
     {
-        IList<Item> Items;
+        public IList<Item> Items { get; private set; }
+
+
+        public Program()
+        {
+            Items = CreateItemList();
+        }
+
+
         static void Main(string[] args)
         {
             System.Console.WriteLine("OMGHAI!");
 
-            var app = new Program()
-                          {
-                              Items = new List<Item>
-                                          {
-                                              new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                                              new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                                              new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                                              new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                                              new Item
-                                                  {
-                                                      Name = "Backstage passes to a TAFKAL80ETC concert",
-                                                      SellIn = 15,
-                                                      Quality = 20
-                                                  },
-                                              new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-                                          }
-
-                          };
-
+            var app = new Program();
             app.UpdateQuality();
 
             System.Console.ReadKey();
 
         }
+
+
+        public List<Item> CreateItemList()
+        {
+            List<Item> items = new List<Item>
+            {
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 15,
+                    Quality = 20
+                },
+                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+            };
+            return items;
+        }
+
+
 
         public void UpdateQuality()
         {
@@ -112,13 +124,13 @@ namespace GildedRose.Console
 
     }
 
-    public class Item
-    {
-        public string Name { get; set; }
+    //public class Item
+    //{
+    //    public string Name { get; set; }
 
-        public int SellIn { get; set; }
+    //    public int SellIn { get; set; }
 
-        public int Quality { get; set; }
-    }
+    //    public int Quality { get; set; }
+    //}
 
 }
