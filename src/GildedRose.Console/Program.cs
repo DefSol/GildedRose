c1c766b00a409c207ac80f4dde212d4033bbbe48
Kieron Dye <kdye@fraedom.com>
2017-02-14 12:32:09 +1300

    Refactored to make more readable by introducing a method for incrementing quality that checks max and introducing a method for updating quality of a single item.

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -38,98 +38,95 @@ namespace GildedRose.Console
 
         }
 
+        const int MaxQuality = 50;
+
         public void UpdateQuality()
         {
-            const int MaxQuality = 50;
-                 
             foreach(Item item in Items)
             {
-                if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                UpdateQuality(item);
+            }
+        }
+
+        private static void UpdateQuality(Item item)
+        {
+            if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (item.Quality > 0)
                 {
-                    if (item.Quality > 0)
+                    if (item.Name != "Sulfuras, Hand of Ragnaros")
                     {
-                        if (item.Name != "Sulfuras, Hand of Ragnaros")
+                        if (item.Name == "Conjured Mana Cake")
                         {
-                            if (item.Name == "Conjured Mana Cake")
-                            {
-                                item.Quality = item.Quality - 2;
-                            }
-                            else
-                            {
-                                item.Quality = item.Quality - 1;
-                            }
-                            
-                        }
-                    }
-                }
-                else
-                {
-                    if (item.Quality < MaxQuality)
-                    {
-                        item.Quality = item.Quality + 1;
-
-                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (item.SellIn < 11)
-                            {
-                                if (item.Quality < MaxQuality)
-                                {
-                                    item.Quality = item.Quality + 1;
-                                }
-                            }
-
-                            if (item.SellIn < 6)
-                            {
-                                if (item.Quality < MaxQuality)
-                                {
-                                    item.Quality = item.Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (item.Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    item.SellIn = item.SellIn - 1;
-                }
-
-                if (item.SellIn < 0)
-                {
-                    if (item.Name != "Aged Brie")
-                    {
-                        if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (item.Quality > 0)
-                            {
-                                if (item.Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    if (item.Name == "Conjured Mana Cake")
-                                    {
-                                        item.Quality = item.Quality - 2;
-                                    }
-                                    else
-                                    {
-                                        item.Quality = item.Quality - 1;
-                                    }
-                                }
-                            }
+                            item.Quality = item.Quality - 2;
                         }
                         else
                         {
-                            item.Quality = item.Quality - item.Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (item.Quality < MaxQuality)
-                        {
-                            item.Quality = item.Quality + 1;
+                            item.Quality = item.Quality - 1;
                         }
                     }
                 }
             }
+            else
+            {
+                IncrementItemQualityByOne(item);
+
+                if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (item.SellIn < 11)
+                    {
+                        IncrementItemQualityByOne(item);
+                    }
+
+                    if (item.SellIn < 6)
+                    {
+                        IncrementItemQualityByOne(item);
+                    }
+                }
+            }
+
+            if (item.Name != "Sulfuras, Hand of Ragnaros")
+            {
+                item.SellIn = item.SellIn - 1;
+            }
+
+            if (item.SellIn < 0)
+            {
+                if (item.Name != "Aged Brie")
+                {
+                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (item.Quality > 0)
+                        {
+                            if (item.Name != "Sulfuras, Hand of Ragnaros")
+                            {
+                                if (item.Name == "Conjured Mana Cake")
+                                {
+                                    item.Quality = item.Quality - 2;
+                                }
+                                else
+                                {
+                                    item.Quality = item.Quality - 1;
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        item.Quality = item.Quality - item.Quality;
+                    }
+                }
+                else
+                {
+                    IncrementItemQualityByOne(item);
+                }
+            }
         }
 
+        private static void IncrementItemQualityByOne(Item item)
+        {
+            if (item.Quality < MaxQuality)
+                item.Quality = item.Quality + 1;
+        }
     }
 }
